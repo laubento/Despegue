828a3ba5605cf0e7c4cc8dfa8d35b25e97145ea4
bagdadstyle <mau20410@gmail.com>
2022-10-11 19:31:44 -0300

    config express

--- api/index.js
+++ api/index.js
@@ -1,4 +1,7 @@
-const app = require("./src/app") 
-const db = require("./src/db")
+const app = require("./src/app");
+const db = require("./src/db");
 
-app.listen(3000, () => console.log("listening on port 3000"), db())
+app.listen(3000, () => {
+  console.log("listening on port 3000");
+  db();
+});
