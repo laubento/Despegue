14d55d2695d4967c7b41529aa26956ddad14ed6e
bagdadstyle <mau20410@gmail.com>
2022-10-11 18:30:24 -0300

    express config

--- api/Src/app.js
+++ api/Src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
+const index = require("../index");
 require("dotenv").config();
 const { URI, USER, PASSWORD } = process.env;
 
@@ -21,6 +22,7 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   next();
 });
+app.use("/", index);
 
 app.use((err, req, res, next) => {
   // eslint-disable-line no-unused-vars
