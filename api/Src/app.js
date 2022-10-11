4918673a9464382013f161df70eb007c53717865
bagdadstyle <mau20410@gmail.com>
2022-10-11 18:16:46 -0300

    express config

--- api/Src/app.js
+++ api/Src/app.js
@@ -1,10 +1,33 @@
-const express = require("express")
-const mongoose = require("mongoose")
-require("dotenv").config()
-const {URI, USER, PASSWORD} = process.env
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+require("dotenv").config();
+const { URI, USER, PASSWORD } = process.env;
 
-const app = express()
+const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 
+app.use((err, req, res, next) => {
+  // eslint-disable-line no-unused-vars
+  const status = err.status || 500;
+  const message = err.message || err;
+  console.error(err);
+  res.status(status).send(message);
+});
 
-module.exports = app
+module.exports = app;
