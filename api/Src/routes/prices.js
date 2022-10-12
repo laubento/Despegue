f8504a5ada6332163f244686d760f97e7707a901
bagdadstyle <mau20410@gmail.com>
2022-10-12 15:23:43 -0300

    priceFilter

--- api/Src/routes/prices.js
+++ api/Src/routes/prices.js
@@ -4,7 +4,14 @@ const infoFlights = require("../utils/data");
 
 console.log(infoFlights);
 router.post("/", (req, res) => {
-    
+  let { priceMin, priceMax } = req.body;
+  if (priceMin && priceMax) {
+    let filterPrices = infoFlights.filter(
+      (e) => e.price >= priceMin && e.price <= priceMax
+    );
+    res.status(201).send(filterPrices);
+  }
+  res.status(400).send("Se requiere precio minimo y precio maximo");
 });
 
 module.exports = router;
