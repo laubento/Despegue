967bf643475d39b9715c01caa28be9e0f4ccd447
Jonathan Monti <jonathanmonti@Jonathans-MacBook-Pro.local>
2022-10-12 10:31:17 -0300

    some changes

--- client/front/src/index.js
+++ client/front/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './redux/Store.js'
+import {store} from './Redux/Store.js'
 
 ReactDOM.render(
   <Provider store={store}>
