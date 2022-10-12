97ce86970149c23d9b885c0d5f76148d1b82d20d
unknown <tironimateo02@gmail.com>
2022-10-12 20:26:58 -0300

    footer

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import Card from "./components/Card/Card.js";
+// import Card from "./components/Card/Card.js";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 
