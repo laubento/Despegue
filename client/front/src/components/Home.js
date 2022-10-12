9ae40ccea4189a8a867fca29597927a068392714
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-10-12 18:54:44 -0300

    feat/component Card

--- client/front/src/components/Home.js
+++ client/front/src/components/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Card from './Card'
+import info from './data.js'
+import './styles/Flights.css'
+// se usa info de momento
+
 
 function Home() {
+ 
   return (
     <div>
-        <Card />
+      <Link to ='/flights'>
+      <button>Flights</button>
+      </Link>
     </div>
   )
 }
