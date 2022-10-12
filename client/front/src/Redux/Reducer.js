407c5358863d769a92e32582b52f250914b99a89
bagdadstyle <mau20410@gmail.com>
2022-10-12 11:02:18 -0300

    getAllFlights Front

--- client/front/src/Redux/Reducer.js
+++ client/front/src/Redux/Reducer.js
@@ -1,9 +1,18 @@
-const initialState = {
-    flights : []
-}
+import { GET_ALL_FLIGHTS } from "./Actions";
 
-export default function reducer(state = initialState, action){
-    switch(action.type){
-        
-    }
+const initialState = {
+  flights: [],
+  allFlights: [],
+};
+
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_ALL_FLIGHTS:
+      return {
+        ...state,
+        allFlights: action.payload,
+      };
+    default:
+      return state;
+  }
 }
