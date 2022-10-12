a85a53c7a41b0fbc8251537f538d6d25656d388c
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-10-12 19:06:43 -0300

    feat/fix-carpets-front

--- client/front/src/components/Flights/Flights.js
+++ client/front/src/components/Flights/Flights.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import Card from './Card/Card'
-import info from './Data/data.js'
-import './styles/Flights.css'
+import Card from '../Card/Card'
+import info from '../Data/data.js'
+import '../styles/Flights.css'
 // se usa info de momento
 
 
--- client/front/src/components/Home/Home.js
+++ client/front/src/components/Home/Home.js
@@ -1,8 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Card from './Card'
-import info from './data.js'
-import './styles/Flights.css'
 // se usa info de momento
 
 
