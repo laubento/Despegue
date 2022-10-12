395136f24d9d55b9d66457fd5ca9c8a2c53979f4
bagdadstyle <mau20410@gmail.com>
2022-10-12 12:45:44 -0300

    merge

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,17 +1,15 @@
-import './App.css';
-import { BrowserRouter, Switch, Route} from 'react-router-dom';
-import Card from './components/Card';
-import Home from './components/Home.js'
-
-
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Card from "./components/Card";
+import Home from "./components/Home.js";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-      <Switch>
-         <Route exact path ='/' component={Home} /> 
-      </Switch>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
--- client/front/src/index.js
+++ client/front/src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ThemeProvider, CSSReset } from "@chakra-ui/core"
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import {store} from './Redux/Store.js'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./Redux/Store.js";
 
 axios.defaults.baseURL = "http://localhost:3001";
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
+    <ThemeProvider>
+      <React.StrictMode>
+        <CSSReset>
+          <App />
+        </CSSReset>
+      </React.StrictMode>
+    </ThemeProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
