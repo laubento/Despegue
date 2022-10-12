f5fb1f0ce6307f03b130f798ca4e569b666eab9f
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-10-12 12:27:22 -0300

    chakra config

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
-import {BrowserRouter, Route} from 'react-router-dom';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import Card from './components/Card';
+import Home from './components/Home.js'
+
+
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        
+      <Switch>
+         <Route exact path ='/' component={Home} /> 
+      </Switch>
       </div>
     </BrowserRouter>
   );
--- client/front/src/index.js
+++ client/front/src/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { ThemeProvider, CSSReset } from "@chakra-ui/core"
 import './index.css';
 import App from './App';
 import { Provider } from 'react-redux';
-import {store} from './redux/Store.js'
+import { store } from './redux/store.js'
+
+
 
 ReactDOM.render(
   <Provider store={store}>
+    <ThemeProvider>
+
     <React.StrictMode>
+      <CSSReset />
       <App />
     </React.StrictMode>
+    </ThemeProvider>
   </Provider>,
   document.getElementById('root')
 );
