6dcdfac433f17edc1a1d55731e5d3b077a063521
unknown <tironimateo02@gmail.com>
2022-10-12 15:50:54 -0300

    GET detail del vuelo

--- api/Src/routes/index.js
+++ api/Src/routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require("express");
 const router = Router();
 const User = require("../../models/user");
+const axios = require('axios')
 
 router.get("/", async (req, res) => {
     
 });
+
+router.get('/flight/:id')
+
 module.exports = router;
