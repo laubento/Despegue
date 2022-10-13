822d7b6a57a1dcfaf1b7121e38e04ec25dafabe8
bagdadstyle <mau20410@gmail.com>
2022-10-12 21:24:54 -0300

    'Alert-card'

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Home from "./components/Home/Home.js";
 import Flights from "./components/Flights/Flights";
 
--- client/front/src/components/Alert/Alert.js
+++ client/front/src/components/Alert/Alert.js
@@ -2,16 +2,18 @@ import React from "react";
 
 const Alert = ({ alert, msg }) => {
   return (
-    <div className="card-div ">
-      <div className="border-right border-left col text-center">
-        <div className="card-title">
-          <h2>Alert</h2>
-          <span className="font-weight-bold">
-            {msg ? msg : "Vuelo no especificado"}
-          </span>
-        </div>
-        <div>
-          <button>Try again</button>
+    <div className="container m-0 p-0">
+      <div className="card-div border-right border-left">
+        <div className="text-center">
+          <div className="card-title">
+            <h2>Alert</h2>
+            <span className="font-weight-bold">
+              {msg ? msg : "Vuelo no especificado"}
+            </span>
+          </div>
+          <div>
+            <button className="btn-bd-primary">Try again</button>
+          </div>
         </div>
       </div>
     </div>
