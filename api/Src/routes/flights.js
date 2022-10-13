abcc32eec32cab831d22222cb81aad9068356fce
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-12 23:53:13 -0300

    feat/ filters done

--- api/Src/routes/flights.js
+++ api/Src/routes/flights.js
@@ -2,46 +2,50 @@ const { Router } = require("express");
 const utils = require("./utils");
 const router = Router();
 
+const flights = require("../API Mock/data.json");
 // const flights = require('../API Mock/lessData.json')
 
-router.get(
-    "/oneWay",
-    async (req, res) => {
-        // const { stopOversCount, minPrice, maxPrice, maxDuration } = req.query;
-        // console.log(stopOversCount);
-        // console.log(minPrice);
-        // console.log(maxPrice);
-        // // console.log(maxDuration);
+router.get("/oneWay", async (req, res) => {
+    const { stopOversCount, minPrice, maxPrice, maxDuration } = req.query;
 
-        try {
-            const flights = await utils.getFlightsOneWay(req.body);
-            let filteredFlights = flights;
+    const stopOversCountNumber = parseInt(stopOversCount);
+    const minPriceNumber = parseInt(minPrice);
+    const maxPriceNumber = parseInt(maxPrice);
+    const maxDurationNumber = parseInt(maxDuration);
 
-            console.log(filteredFlights);
-            if (stopOversCount) {
-                console.log("gijfisahfuoashbufgh");
-                filteredFlights = filteredFlights.filter((flight) => flight.stopoversCount === stopOversCount);
-            }
-            console.log(filteredFlights);
-            if (minPrice && maxPrice) {
-                console.log("mvbncxmbvnmczxbvnmcxb");
-                filteredFlights = filteredFlights.filter((flight) => flight.price >= minPrice && flight.price <= maxPrice);
-            }
-            console.log(filteredFlights);
-            if (maxDuration) {
-                filteredFlights = filteredFlights.filter(
-                    (flight) => flight.duration < maxDuration
-                );
-            }
+    try {
+        // const flights = await utils.getFlightsOneWay(req.body);
+        let filteredFlights = flights;
 
-            res.status(200).send(flights);
-        } catch (error) {
-            console.log(error);
-
-            res.status(404).send({ error: error.response.data.message ? error.response.data.message : error});
+        if (stopOversCount) {
+            filteredFlights = filteredFlights.filter(
+                (flight) => flight.stopoversCount === stopOversCountNumber
+            );
         }
+        if (minPrice && maxPrice) {
+            filteredFlights = filteredFlights.filter(
+                (flight) =>
+                    flight.price >= minPriceNumber &&
+                    flight.price <= maxPriceNumber
+            );
+        }
+        if (maxDuration) {
+            filteredFlights = filteredFlights.filter(
+                (flight) => flight.duration < maxDurationNumber
+            );
+        }
+
+        res.status(200).send(filteredFlights);
+    } catch (error) {
+        console.log(error);
+
+        res.status(404).send({
+            error: error.response.data.message
+                ? error.response.data.message
+                : error,
+        });
     }
-);
+});
 
 router.get("/roundTrip", async (req, res) => {
     try {
