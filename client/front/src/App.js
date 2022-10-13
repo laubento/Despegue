9f2b1a7c62db12d14c1026d5d2c3cd39942f72b4
unknown <tironimateo02@gmail.com>
2022-10-12 22:12:39 -0300

    Footer

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -2,15 +2,17 @@ import "./App.css";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from "./components/Home/Home.js";
 import Flights from "./components/Flights/Flights";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
+    <BrowserRouter>
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/flights" component={Flights} />
-        </Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/flights" component={Flights} />
+        <Route path="/" component={Footer} />
       </div>
+    </BrowserRouter>
   );
 }
 
