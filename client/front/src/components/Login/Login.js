f068d8c5b4cf79117e536e54e3b4c8dddcaba382
bagdadstyle <mau20410@gmail.com>
2022-10-13 12:48:47 -0300

    login

--- client/front/src/components/Login/Login.js
+++ client/front/src/components/Login/Login.js
@@ -2,32 +2,42 @@ import React, { useState } from "react";
 import axios from "axios";
 
 const Login = () => {
-  const [regUserName, setRegUsername] = useState("");
+  const [regUser, setRegUser] = useState({ name: "", password: "" });
   const [regPassword, setRegPassword] = useState("");
   const [logUserName, setLogUsername] = useState("");
   const [logPassword, setLogPassword] = useState("");
   const register = () => {
-    axios({
-      method: "post",
-      data: {
-        username: regUserName,
-        password: regPassword,
-      },
+    axios.post("/register", regUser);
+  };
+
+  const login = () => {};
+  const getUser = () => {};
+  const handleChange = (e) => {
+    e.preventDefault();
+    setRegUser({
+      ...regUser,
+      [e.target.name]: e.target.value,
     });
   };
-  const login = () => {
-    axios.post("/login", logUserName);
-  };
-  const getUser = () => {};
-
+  console.log(regUser);
   return (
     <div>
       <div>
         <h2>Register</h2>
         <label>User</label>
-        <input type="text" onChange={(e) => setRegUsername(e.target.value)} />
+        <input
+          type="text"
+          name="name"
+          value={regUser.name}
+          onChange={handleChange}
+        />
         <label>Password</label>
-        <input type="text" onChange={(e) => setRegPassword(e.target.value)} />
+        <input
+          type="text"
+          name="password"
+          value={regUser.password}
+          onChange={handleChange}
+        />
         <button onClick={register}>Submit</button>
       </div>
       <div>
