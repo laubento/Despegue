8f19aab31f339c808517987850899196f5bdaa37
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-12 21:29:52 -0300

    backup

---
+++ api/Src/routes/filters.js
@@ -0,0 +1,9 @@
+// const {Router} = require('express')
+// const utils = require('./utils')
+// const router = Router()
+
+
+// router.get('/')
+
+
+// module.exports = router
--- api/Src/routes/index.js
+++ api/Src/routes/index.js
@@ -4,8 +4,8 @@ const router = Router();
 const User = require("../../models/user");
 const axios = require('axios')
 
-const prices = require("./filters/prices");
-router.use('/prices', prices)
+// const prices = require("./filters/prices");
+// router.use('/prices', prices)
 
 const flightRouter = require("./flights")
 router.use('/flights', flightRouter)
@@ -15,5 +15,8 @@ router.use('/stopOvers', stopOversFilter)
 
 const flightDurationFilter = require('./filters/duration')
 router.use('/duration', flightDurationFilter)
+
+// const filters = require('./filters')
+// router.use('/filters')
 
 module.exports = router;
