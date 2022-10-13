75c8f6b2362fcc9efed0c45018fc7cc06ee3603a
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-13 12:25:53 -0300

    redux filters done + roundtrip fixes

--- client/front/src/Redux/Actions.js
+++ client/front/src/Redux/Actions.js
@@ -1,18 +1,28 @@
 import axios from 'axios';
 
+export const GET_FLIGHTS = "GET_FLIGHTS"
+export const FILTER_FLIGHTS = "FILTER_FLIGHTS"
+
 export function getFlights(flight){
     const tripType = flight.tripType;
 
     return async (dispatch) => {
         // var json = await axios.get(`https://api.flightapi.io/${tripType}/${apiKey}/${from}/${to}/${depart}/${adults}/${children}/${infants}/${cabinClass}/${currency}`)
-        const flights = await axios.post(`http://localhost:3001/flights/${tripType}`, {flight})
+        const flights = await axios.post(`/flights/${tripType}`, {flight})
 
         return dispatch({
-            type: 'GET_FLIGHTS',
-            payload: flights
+            type: GET_FLIGHTS,
+            payload: flights.data
         })
     }
-
-
 }
 
+// filters = estado de los botones
+export const filterFlights = (filters) => {
+    return function(dispatch){
+        dispatch({
+            type: FILTER_FLIGHTS,
+            payload: filters
+        })
+    }
+}
--- client/front/src/Redux/Reducer.js
+++ client/front/src/Redux/Reducer.js
@@ -1,23 +1,51 @@
-// import { GET_ALL_FLIGHTS } from "./Actions";
+import { GET_FLIGHTS, FILTER_FLIGHTS } from "./Actions";
 
 const initialState = {
-  flights: [],
-  allFlights: [],
+    flights: [],
+    allFlights: [],
+    filteredFlights: [],
 };
 
 export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    // case GET_ALL_FLIGHTS:
-    //   return {
-    //     ...state,
-    //     allFlights: action.payload,
-    //   };
-    case 'GET_FLIGHTS':
-      return{
-          ...state,
-          flights: action.payload
-      }
-    default:
-      return state;
-  }
+    switch (action.type) {
+        // case GET_ALL_FLIGHTS:
+        //   return {
+        //     ...state,
+        //     allFlights: action.payload,
+        //   };
+        case GET_FLIGHTS:
+            return {
+                ...state,
+                flights: action.payload,
+            };
+        case FILTER_FLIGHTS:
+            const { minPrice, maxPrice, maxDuration, stopOvers } =
+                action.payload;
+
+            let filteringFlights = state.flights.slice();
+
+            if (stopOvers !== "default") {
+                filteringFlights = filteringFlights.filter(
+                    (flight) => flight.stopoversCount === stopOvers
+                );
+            }
+            if (minPrice !== "default" && maxPrice !== "default") {
+                filteringFlights = filteringFlights.filter(
+                    (flight) =>
+                        flight.price >= minPrice && flight.price <= maxPrice
+                );
+            }
+            if (maxDuration !== "default") {
+                filteringFlights = filteringFlights.filter(
+                    (flight) => flight.duration < maxDuration
+                );
+            }
+            return {
+                ...state,
+                filteredFlights: filteringFlights,
+            };
+
+        default:
+            return state;
+    }
 }
