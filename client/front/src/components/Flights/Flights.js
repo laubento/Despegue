eb3630323382fe92892e34139c3ee7cdcc78f52e
unknown <tironimateo02@gmail.com>
2022-10-13 17:27:41 -0300

    filtros

--- client/front/src/components/Flights/Flights.js
+++ client/front/src/components/Flights/Flights.js
@@ -1,17 +1,29 @@
-import React from "react";
-import info from "../Data/data.js";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+// import info from "../Data/data.js";
+import Filter from "../Filter/Filter.js";
 import Paginado from "../Paginado/Paginado";
 import "../styles/Flights.css";
+
 // se usa info de momento
 
 function Flights() {
+  let info = useSelector(state => state.flights)
+  let infofiltrada = useSelector(state => state.filteredFlights)
+  // console.log(infofiltrada)
+  if(infofiltrada.length > 0){
+    info = infofiltrada
+  }
 
   return (
-    <div className="flights-cont position-relative">
-      <div className="flights-container-cards">
+    <div className="d-flex">
+      <Filter />
+      <div className="flights-cont position-relative ms-5">
+        <div className="flights-container-cards">
           <Paginado
             data={info}
           />
+        </div>
       </div>
     </div>
   );
