7972a2da65ac167b0184acd89deb4ea3ca5434f2
bagdadstyle <mau20410@gmail.com>
2022-10-13 00:35:10 -0300

    develop

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -1,19 +1,17 @@
 import "./App.css";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 // import Card from "./components/Card/Card.js";
 import Home from "./components/Home/Home";
 import Footer from "./components/Footer/Footer";
 import Flights from "./components/Flights/Flights";
-import Footer from "./components/Footer/Footer";
 import FlightsSearch from "./components/FlightsSearch/FlightsSearch";
 
 function App() {
   return (
-
     <div className="App">
       <BrowserRouter>
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/flightSearch" component={FlightsSearch}/>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/flightSearch" component={FlightsSearch} />
         <Route path="/flights" component={Flights} />
         <Route path="/" component={Footer} />
       </BrowserRouter>
