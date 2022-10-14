33d78e79aca82f0e66ff226e69025688a996eaae
bagdadstyle <mau20410@gmail.com>
2022-10-14 20:30:22 -0300

    develop fix

--- client/front/src/components/Home/Home.js
+++ client/front/src/components/Home/Home.js
@@ -1,14 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import FlightsSearch from "../FlightsSearch/FlightsSearch";
-import LogReg from "../Login/Formulario/LogReg";
 
 // se usa info de momento
 
 function Home({ user }) {
   return (
     <div>
-      {/* <LogReg /> */}
       <h1>ASDASDASD</h1>
       <Link to="/flights">
         <button>Flights</button>
--- client/front/src/components/Login/Login.js
+++ client/front/src/components/Login/Login.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
-import '../styles/Login.css'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import "../styles/Login.css";
+import { Formik, Form, Field, ErrorMessage } from "formik";
 
 const Login = () => {
   const [formularioEnviado, cambiarFormularioEnviado] = useState(false);
@@ -48,67 +48,82 @@ const Login = () => {
     });
   };
   return (
-<>
-			<Formik
-				initialValues={{
-					password: '',
-					email: ''
-				}}
-				validate={(valores) => {
-					let errores = {};
+    <>
+      <Formik
+        initialValues={{
+          password: "",
+          email: "",
+        }}
+        validate={(valores) => {
+          let errores = {};
 
-					// Validacion nombre
-					if(!valores.password){
-						errores.password = 'Please enter a password'
-					} 
+          // Validacion nombre
+          if (!valores.password) {
+            errores.password = "Please enter a password";
+          }
 
-					// Validacion correo
-					if(!valores.email){
-						errores.email = 'Please enter an email address'
-					} else if(!/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(valores.email)){
-						errores.email = 'Mail can only contain letters, numbers, periods, hyphens and underscores'
-					}
+          // Validacion correo
+          if (!valores.email) {
+            errores.email = "Please enter an email address";
+          } else if (
+            !/^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/.test(
+              valores.email
+            )
+          ) {
+            errores.email =
+              "Mail can only contain letters, numbers, periods, hyphens and underscores";
+          }
 
-					return errores;
-				}}
-				onSubmit={(valores, {resetForm}) => {
-					resetForm();
-					console.log('Form submitted');
-					cambiarFormularioEnviado(true);
-					setTimeout(() => cambiarFormularioEnviado(false), 5000);
-          login(valores)
-				}}
-			>
-				{( {errors} ) => (
+          return errores;
+        }}
+        onSubmit={(valores, { resetForm }) => {
+          resetForm();
+          console.log("Form submitted");
+          cambiarFormularioEnviado(true);
+          setTimeout(() => cambiarFormularioEnviado(false), 5000);
+          login(valores);
+        }}
+      >
+        {({ errors }) => (
           <div className="Login-containerPrincipal">
             <Form className="Login-formulario">
               <div>
                 <label htmlFor="email">Correo</label>
                 <Field
-                  type="text" 
-                  id="email" 
-                  name="email" 
-                  placeholder="abc@email.com" 
+                  type="text"
+                  id="email"
+                  name="email"
+                  placeholder="abc@email.com"
                 />
-                <ErrorMessage name="email" component={() => (<div className="error">{errors.email}</div>)} />
+                <ErrorMessage
+                  name="email"
+                  component={() => <div className="error">{errors.email}</div>}
+                />
               </div>
               <div>
                 <label htmlFor="name">Password</label>
                 <Field
-                  type="password" 
-                  id="password" 
-                  name="password" 
+                  type="password"
+                  id="password"
+                  name="password"
                   placeholder="********"
                 />
-                <ErrorMessage name="password" component={() => (<div className="error">{errors.password}</div>)} />
+                <ErrorMessage
+                  name="password"
+                  component={() => (
+                    <div className="error">{errors.password}</div>
+                  )}
+                />
               </div>
               <button type="submit">Enviar</button>
-              {formularioEnviado && <p className="exito">Formulario enviado con exito!</p>}
+              {formularioEnviado && (
+                <p className="exito">Formulario enviado con exito!</p>
+              )}
             </Form>
           </div>
-				)}
-			</Formik>
-		</>
+        )}
+      </Formik>
+    </>
   );
 };
 
