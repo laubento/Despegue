e949affe8f708616a1b36ffd707636d24d15ae4b
bagdadstyle <mau20410@gmail.com>
2022-10-13 21:34:08 -0300

    login-tofix

--- api/Src/routes/login/register.js
+++ api/Src/routes/login/register.js
@@ -3,45 +3,23 @@ const User = require("../../../models/user");
 const bcrypt = require("bcryptjs");
 const router = Router();
 
-// router.post("/", async (req, res) => {
-// const {name, email, password, confirm_password} = req.body
+router.post("/", (req, res) => {
+  User.findOne({ name: req.body.name }, async (err, doc) => {
+    if (err) throw err;
+    console.log(doc);
+    if (doc) res.status(400).send("User Already Exists");
+    if (!doc) {
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
-// if(password !== confirm_password){
-//   return res.send('Contraseñas distintas.')
-// }
-
-// const newUser = new User({name,email,password})
-
-// await newUser.save();
-// const usuarios = User.find({})
-// console.log(usuarios)
-// console.log(req.body)
-
-// const router = Router();
-
-router.post("/", async (req, res) => {
-  // const user = await User.findOne({ email: req.body.email });
-  // if (req.body.emial === user.email) res.send("Usuario existente");
-  User.findOne(
-    {
-      name: req.body.name,
-    },
-    async (err, doc) => {
-      if (err) throw err;
-      if (doc) return res.send("User already exists");
-      if (!doc) {
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        const newUser = new User({
-          name: req.body.name,
-          password: hashedPassword,
-          email: req.body.email,
-        });
-        await newUser.save();
-        return res.status(201).send("User create");
-      }
+      const newUser = new User({
+        name: req.body.name,
+        password: hashedPassword,
+        email: req.body.email,
+      });
+      await newUser.save();
+      res.send("User Created");
     }
-  );
+  });
 });
 
 module.exports = router;
--- api/models/user.js
+++ api/models/user.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
 
 const User = mongoose.Schema({
-  username: {
+  name: {
     type: String,
     required: true,
   },
