4734787133ba1571583fe8333c270590aa230994
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-10-16 18:58:18 -0300

    fix paginado

--- client/front/src/components/Paginado/Paginado.js
+++ client/front/src/components/Paginado/Paginado.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from "react";
-import ReactPaginate from "react-paginate";
-import Card from "../Card/Card";
-import Loader from "../Loader/Loader";
-=======
 import React from "react";
 import { Link } from 'react-router-dom'
->>>>>>> feat/input-de-busqueda-aiport
 import "../styles/Paginado.css";
 
 function Paginado({ paginado, allFlights, flightsPerPage, prevHandler, nextHandler }) {
@@ -15,60 +8,7 @@ function Paginado({ paginado, allFlights, flightsPerPage, prevHandler, nextHandl
       pageNumbers.push(i)
   }
 
-//   const prevHandler = (e) => {
-//     e.preventDefault()
-//     if(number === pageNumbers[0]) return;
-//     number--
-//     let prevPage = number
-//     return paginado(prevPage)
-// }
-
-// const nextHandler = (e) => {
-//     e.preventDefault()
-//     if(number === pageNumbers.length) return;
-//     number++
-//     let nextPage = number
-//   return paginado(nextPage)
-// }
-
   return (
-<<<<<<< HEAD
-    <>
-      <div className=" ">
-        {currentItems.length !== 0 ? currentItems.map((e, i) => {
-          return (
-            <div key={i} className="p">
-              <Card
-                key={e.id}
-                id={e.id}
-                airlinesName={e.airlinesNames}
-                departureTime={e.departureTime}
-                arrivalTime={e.arrivalTime}
-                duration={e.duration}
-                stopoversCount={e.stopoversCount}
-                price={e.price}
-              />
-            </div>
-          );
-        }): <Loader/>}
-      </div>
-      <ReactPaginate
-        breakLabel="..."
-        nextLabel=" >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={pageCount}
-        previousLabel="< "
-        renderOnZeroPageCount={null}
-        containerClassName="pagination position-absolute fs-4 top-40 start-50 translate-middle"
-        pageLinkClassName="page-num"
-        previousLinkClassName="page-num"
-        nextLinkClassName="page-num"
-        activeLinkClassName="active fs-3 p-2"
-      />
-    </>
-  );
-=======
   <nav className="w-100 p-4 pag-cont"> 
     <ul className="pag-nav">
     <li className="flights-li" onClick={(e) => prevHandler(e) }>{'<'}</li>
@@ -84,7 +24,6 @@ function Paginado({ paginado, allFlights, flightsPerPage, prevHandler, nextHandl
       </ul>
   </nav>
 )
->>>>>>> feat/input-de-busqueda-aiport
 }
 
 export default Paginado;
