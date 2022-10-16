736bb0c84b7b8091bef39a69bb932efcd5f4dd39
unknown <tironimateo02@gmail.com>
2022-10-16 18:19:42 -0300

    img Loader

--- client/front/src/components/Loader/Loader.js
+++ client/front/src/components/Loader/Loader.js
@@ -6,10 +6,6 @@ export default function Loader() {
     return (
         <div className="p-4">
             <img src={img} alt="Loader Img"/>
-            <button class="btn btn-primary" type="button" disabled>
-                <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
-                Loading...
-            </button>
         </div>
     );
 }
