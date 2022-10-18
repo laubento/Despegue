16677ac4fe4bc2691a0a31c6588755e9dc12b424
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-18 18:19:23 -0300

    a

--- api/Src/app.js
+++ api/Src/app.js
@@ -16,13 +16,14 @@ require("./routes/login/passportConfig");
 
 console.log(process.env.VERCEL_URL);
 
-// app.use(
-//     cors({
-//         origin: process.env.VERCEL_URL || "http://localhost:3000", // <-- location of the react app were connecting to
-//         methods: "GET,POST,PUT,DELETE",
-//         credentials: true,
-//     })
-// );
+app.use(
+    cors({
+        origin: process.env.VERCEL_URL || "http://localhost:3000", // <-- location of the react app were connecting to
+        methods: "GET,POST,PUT,DELETE",
+        credentials: true,
+        allowedHeaders: "Content-Type, Authorization"
+    })
+);
 
 app.use(
     cookieSession({
@@ -35,16 +36,16 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(morgan("dev"));
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", ""); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 // app.use(
 //   cors({
