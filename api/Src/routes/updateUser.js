36088d58ca081acdda09970eba5971e5ae715dc7
kalinxtr <nehuenxtreem6@gmail.com>
2022-10-18 14:35:03 -0300

    Ruta utilizable

--- api/Src/routes/updateUser.js
+++ api/Src/routes/updateUser.js
@@ -3,8 +3,10 @@ const router = Router();
 const User = require("../../models/user")
 
 router.put("/", async (req, res) => {
+    const {id, firstName, lastName, birthDate, dni, phone, email} = req.body
     try{
-        const updateUser = await User.updateOne({_id: req.body._id}, {$set: {name: req.body.name}})
+        const updateUser = await User.updateOne({_id: id}, {$set: {firstName: firstName , lastname: lastName, dni: dni,
+             phone: phone, email: email, birthDate: birthDate}})
         res.send(updateUser)
 }   
     catch(err){console.log(err)}
