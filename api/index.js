89e27de72fb6680debe753a7066e5211f99170f6
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-18 15:24:46 -0300

    d

--- api/index.js
+++ api/index.js
@@ -1,5 +1,5 @@
-const app = require("./src/app.js");
-const db = require("./src/db.js");
+const app = require("./Src/app.js");
+const db = require("./Src/db.js");
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("listening on port 3001");
