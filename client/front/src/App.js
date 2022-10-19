1f3710dfcad177a06b099533d3614d10b0ea556f
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-19 20:47:39 -0300

    a

--- client/front/src/App.js
+++ client/front/src/App.js
@@ -20,6 +20,7 @@ import { useState } from "react";
 import axios from "axios";
 import { useAuth0 } from "@auth0/auth0-react";
 
+
 function App() {
     const dispatch = useDispatch();
     //   useEffect(() => {
