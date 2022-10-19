f6263228e11d39d1440d8fb0ade0ae8c804163dc
bagdadstyle <mau20410@gmail.com>
2022-10-19 18:02:17 -0300

    jwt

--- api/index.js
+++ api/index.js
@@ -1,5 +1,25 @@
 const app = require("./Src/app.js");
 const db = require("./Src/db.js");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: "https://dev-ug1ke37sta0b1so8.us.auth0.com/.well-known/jwks.json",
+  }),
+  audience: "https:///www.despegue-api.com",
+  issuer: "https://dev-ug1ke37sta0b1so8.us.auth0.com/",
+  algorithms: ["RS256"],
+});
+
+app.use(jwtCheck);
+
+app.get("/authorized", (req, res) =>{
+  res.send("Secured Resource");
+});
 
 app.listen(process.env.PORT || 3001, () => {
   console.log("listening on port 3001");
