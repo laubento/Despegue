a35369c4aaad744f25ed6f52b08d7ab62d8c3226
Santiago Caballero <saantyc12.caballero@gmail.com>
2022-10-22 17:22:13 -0300

    feat/template-email-compra

--- api/Src/routes/sendMailCompra/sendMailCompra.js
+++ api/Src/routes/sendMailCompra/sendMailCompra.js
@@ -6,10 +6,7 @@ const infoTransporter = require('../utils/mailer.js')
 
 
 router.post('/:id/:payment_id', async (req,res) => {
-    const { id } = req.params
-    const { payment_id } = req.params
-    console.log(id)
-    console.log(payment_id)
+    const { id, payment_id} = req.params
     let users = await User.find({_id: id})
     console.log(users)
     let mapEmail = users.map(e => e.email).join('')
@@ -17,12 +14,15 @@ router.post('/:id/:payment_id', async (req,res) => {
 
     
     // fromTitle, tolist, subject, html
-    await infoTransporter('DESPEGUE', mapEmail, 'Compra de su pasaje', `<div><h1> ${nombre} Gracias por la compra! ✈️✈️ </h1>
-                                                                            <br/>
-                                                                            <h3>Código de pago: ${payment_id}</h3> 
-                                                                            <br/>
-                                                                            <h2 style="color:blueviolet;">DESPEGUE</h2></div>`)
-    res.json(users)
+    await infoTransporter('DESPEGUE', mapEmail, 'Compra de su pasaje', `<div style="background-color: rgb(129, 32, 168); border: 2px solid rgb(227, 118, 241); padding: 20px; margin: 20px; font-family:'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; font-weight:bold">
+                                                                            <h1 style="color: white; text-align:center"> ${nombre}, Gracias por tu compra, buen viaje! ✈️✈️</h1>
+                                                                            <div style="display:flex ; justify-content: center; align-items: center">
+                                                                            <img style="  object-fit: cover; width:100%; height:100%;" src="https://amp.umd.edu/sites/default/files/2021-03/Travel%20Modernization%20Banner.jpg" >
+                                                                            </div>
+                                                                            <h3 style="color: white">Código de pago: ${payment_id}</h3>
+                                                                            <h4 style="color: rgb(223, 176, 227)">att: DESPEGUE.</h4>
+                                                                        </div>`)
+    res.send('msj enviado')
 })
 
 
