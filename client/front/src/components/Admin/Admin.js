b8aa2eae6bc0a0a2ea9c5659174a0724dc8c4c44
unknown <tironimateo02@gmail.com>
2022-10-25 16:51:29 -0300

    Delete Offers, validaciones

--- client/front/src/components/Admin/Admin.js
+++ client/front/src/components/Admin/Admin.js
@@ -74,27 +74,49 @@ export default function Admin() {
         {
             title: 'Imagen',
             field: 'image',
+            validate: rowData => {
+                if(rowData.image === '' || !rowData.image) return 'Required'
+                return true;
+            }
             // editable: false,
         },
         {
             title: 'Cant. Dias',
             field: 'day',
+            type: 'numeric',
+            validate: rowData => {
+                if(rowData.day === '' || !rowData.day) return 'Required'
+                return true;
+            },
             // editable: false,
         },
         {
             title: 'Cant. Noches',
             field: 'nigth',
+            type: 'numeric',
+            validate: rowData => {
+                if(rowData.nigth === '' || !rowData.nigth) return 'Required'
+                return true;
+            },
             // editable: false,
         },
         {
             title: 'Nombre del pais',
             field: 'name',
+            validate: rowData => {
+                if(rowData.name === '' || !rowData.name) return 'Required'
+                return true;
+            },
             // type: 'numeric',
             // editable: false,
         },
         {
             title: 'Dia de salida',
             field: 'dateFrom',
+            validate: rowData => {
+                if(rowData.dateFrom === '' || !rowData.dateFrom) return 'Required'
+                return true;
+            },
             // type: 'time',
             // editable: false,
 
@@ -102,6 +124,10 @@ export default function Admin() {
         {
             title: 'Dia de vuelta',
             field: 'dateTo',
+            validate: rowData => {
+                if(rowData.dateTo === '' || !rowData.dateTo) return 'Required'
+                return true;
+            },
             // type: 'time',
             // editable: false,
 
@@ -109,11 +135,21 @@ export default function Admin() {
         {
             title: 'Aeropuerto de salida',
             field: 'nameAirportFrom',
+            validate: rowData => {
+                if(rowData.nameAirportFrom === '' || !rowData.nameAirportFrom) return 'Required';
+                else if (rowData.nameAirportFrom.length !== 3) return 'Tiene que contener 3 letras'
+                return true;
+            },
             // type: 'time',
             // editable: false,
         }, {
             title: 'Aeropuerto de llegada',
             field: 'nameAirportTo',
+            validate: rowData => {
+                if(rowData.nameAirportTo === '' || !rowData.nameAirportTo) return 'Required';
+                else if (rowData.nameAirportFrom.length !== 3) return 'Tiene que contener 3 letras'
+                return true;
+            },
             // type: 'time',
             // editable: false,
         },
@@ -121,17 +157,29 @@ export default function Admin() {
             title: 'Asistencia',
             field: 'asistans',
             lookup: { Estandar: 'Estandar', Basica: 'Basica', Premium: 'Premium', },
+            validate: rowData => {
+                if(rowData.asistans === '' || !rowData.asistans) return 'Required'
+                return true;
+            },
             // type: 'currency'
         },
         {
             title: 'Rating',
             field: 'rating',
             lookup: { 1: 1, 2: 2, 3: 3, 4: 4, 5: 5 },
+            validate: rowData => {
+                if(rowData.rating === '' || !rowData.rating) return 'Required'
+                return true;
+            },
         },
         {
             title: 'Precio',
             field: 'price',
             type: 'currency',
+            validate: rowData => {
+                if(rowData.price === '' || !rowData.price) return 'Required'
+                return true;
+            },
             // editable: false,
         },
         // {
