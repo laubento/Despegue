7fc27e6565c6c2256deb61ab1b39fccc91c173e5
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-28 14:16:21 -0300

    a

--- api/Src/app.js
+++ api/Src/app.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 // );
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', 'https://despegue.vercel.app/'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
