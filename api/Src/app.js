1612b7eab84f53ad95b7b9556e7bfb7ff14b997b
Ignacio20022 <nacho-bera-02@hotmail.com>
2022-10-28 17:53:28 -0300

    a

--- api/Src/app.js
+++ api/Src/app.js
@@ -36,7 +36,7 @@ app.use(passport.session());
 // );
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
+    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
     res.header('Access-Control-Allow-Credentials', 'true');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
