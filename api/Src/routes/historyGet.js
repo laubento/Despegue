d2914eaca160557e2a7345df97c8651c3c57bfce
bagdadstyle <mau20410@gmail.com>
2022-10-30 20:32:12 -0300

    e

--- api/Src/routes/historyGet.js
+++ api/Src/routes/historyGet.js
@@ -1,18 +1,20 @@
-const {Router} = require("express")
-const User = require("../../models/user")
-const router = Router()
-const History = require("../../models/history")
-const mongoose = require("mongoose")
-const axios = require('axios')
+const { Router } = require("express");
+const User = require("../../models/user");
+const router = Router();
+const History = require("../../models/history");
+const mongoose = require("mongoose");
+const axios = require("axios");
 
-router.get("/getHistory", async (req,res) => {
-    const {id} = req.query
-  
-    History.find({userId:id}).then((data) => {
-        res.status(200).send(data)
-    }).catch((err) => {
-        res.status(200).send([])
+router.get("/getHistory", async (req, res) => {
+  const { id } = req.query;
+
+  History.find({ userId: id })
+    .then((data) => {
+      res.status(200).send(data);
     })
-})
+    .catch((err) => {
+      res.status(200).send([]);
+    });
+});
 
-module.exports= router
+module.exports = router;
