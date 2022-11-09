46fe90818812ba9728b6dc26ff053260fe529669
Ignacio Berasategui <92552909+Ignacio20022@users.noreply.github.com>
2022-11-09 18:11:59 -0300

    Update utils.js

--- api/Src/routes/utils.js
+++ api/Src/routes/utils.js
@@ -1,6 +1,6 @@
 const axios = require('axios');
 require("dotenv").config()
-const API_KEY = "6362ef8583242820f1c766f0"
+const API_KEY = "6363230fe750be20e3895e90"
 
 async function getFlightsOneWay({ departurePlace, arrivalPlace, departureDate, returningDate, adults, childern, infants, cabinClass, currency }) {
     const data = await axios
